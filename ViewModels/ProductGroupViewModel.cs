23a7ebba95a66dff665b46c96c80db5fadcc1f46
Sebastian Nasuta <sebastian.nasuta@gmail.com>
2025-01-16 10:14:53 +0100

    Add delete functionality and UI updates
    Modified MainPage.xaml to change button font size from "Large" to "Medium". Updated ProductGroupPage.xaml to replace StackLayout with SwipeView for product deletion, and replaced StackLayout with Grid for layout consistency. Added new color resource `ExtremeRed` to Colors.xaml. Updated ProductGroupViewModel.cs to include DeleteProductCommand for removing products from the collection.

--- ViewModels/ProductGroupViewModel.cs
+++ ViewModels/ProductGroupViewModel.cs
@@ -1,11 +1,23 @@
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Picart.Models;
 
 namespace Picart.ViewModels
 {
-    internal partial class ProductGroupViewModel(ProductGroup productGroup) : ViewModelBase
+    internal partial class ProductGroupViewModel : ViewModelBase
     {
-        public string GroupName { get; } = productGroup.Name;
-        public ObservableCollection<Product> Products { get; } = new ObservableCollection<Product>(productGroup.Products);
+        public string GroupName { get; }
+        public ObservableCollection<Product> Products { get; }
+
+        public Command<Product> DeleteProductCommand { get; }
+
+        public ProductGroupViewModel(ProductGroup productGroup)
+        {
+            GroupName = productGroup.Name;
+            Products = new ObservableCollection<Product>(productGroup.Products);
+            DeleteProductCommand = new Command<Product>(DeleteProduct);
+        }
+
+        private void DeleteProduct(Product product) => Products.Remove(product);
     }
 }
